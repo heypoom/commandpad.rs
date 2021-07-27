b0a996d8430c891afc130c20cfe829dd8c7500f5
Phoomparin Mano <poom@hey.com>
2021-07-28 04:28:34 +0700

    feat: flappy

--- src/main.rs
+++ src/main.rs
@@ -1,287 +1,45 @@
-extern crate midir;
-extern crate midly;
+mod flappy;
+mod launchpad;
 
 #[macro_use]
 extern crate text_io;
+extern crate midir;
 
-use midir::MidiInput;
-use midir::MidiInputConnection;
-use midir::MidiInputPort;
-use midir::MidiOutputConnection;
-use midir::{MidiOutput, MidiOutputPort};
+use crate::flappy::Flappy;
+use crate::launchpad::{blank_rgb_canvas, get_midi_in_ports, midi_input, rand_u8, Launchpad};
+use std::sync::mpsc::channel;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
+
 use rand::Rng;
-use std::collections::HashMap;
-use std::marker::PhantomData;
 use std::thread::sleep;
 use std::time::Duration;
-
-use std::io::stdout;
-use std::io::Write;
-
-fn midi_input(text: &'static str) -> MidiInput {
-	MidiInput::new(text).unwrap()
-}
-
-fn midi_output(text: &'static str) -> MidiOutput {
-	MidiOutput::new(text).unwrap()
-}
-
-fn print(s: &str) {
-	print!("{}", s);
-	stdout().flush().unwrap();
-}
-
-fn input(prompt: &str) -> String {
-	print(prompt);
-	read!("{}\n")
-}
-
-fn input_usize(prompt: &str) -> usize {
-	print(prompt);
-	read!("{}\n")
-}
-
-fn position_to_note(x: u8, y: u8) -> u8 {
-	81 - (10 * y) + x
-}
-
-fn get_midi_in_ports(conn: &MidiInput) -> (MidiInputPort, MidiInputPort) {
-	let in_ports = conn.ports();
-
-	for port in in_ports.clone() {
-		println!("{:?}", conn.port_name(&port).unwrap());
-	}
-
-	let get_port = move |name: &'static str| {
-		in_ports
-			.clone()
-			.into_iter()
-			.find(|p| conn.port_name(p).unwrap() == name)
-			.unwrap()
-	};
-
-	let daw_conn = get_port("Launchpad X LPX DAW Out");
-	let midi_out = get_port("Launchpad X LPX MIDI Out");
-
-	(daw_conn, midi_out)
-}
-
-fn get_midi_out_ports(out: &MidiOutput) -> (MidiOutputPort, MidiOutputPort) {
-	let out_ports = out.ports();
-
-	let get_port = move |name: &'static str| {
-		out_ports
-			.clone()
-			.into_iter()
-			.find(|p| out.port_name(p).unwrap() == name)
-			.unwrap()
-	};
-
-	let daw_conn = get_port("Launchpad X LPX DAW In");
-	let midi_out = get_port("Launchpad X LPX MIDI In");
-
-	(daw_conn, midi_out)
-}
-
-pub fn initialize_output() -> (MidiOutputConnection, MidiOutputConnection) {
-	let out = midi_output("CommandPad DAW Output");
-	let (daw_port, midi_port) = get_midi_out_ports(&out);
-
-	let daw_conn = out.connect(&daw_port, "commandpad-daw-out").unwrap();
-
-	let midi_conn = {
-		let midi_out = midi_output("CommandPad MIDI Output");
-
-		midi_out.connect(&midi_port, "commandpad-midi-out").unwrap()
-	};
-
-	(daw_conn, midi_conn)
-}
-
-pub struct Launchpad {
-	color_map: HashMap<u8, u8>,
-
-	daw_conn: MidiOutputConnection,
-	midi_conn: MidiOutputConnection,
-
-	midi_in: Option<MidiInputConnection<()>>,
-}
-
-fn empty_pad_state() -> HashMap<u8, u8> {
-	let mut hm = HashMap::new();
-
-	for i in 11..100 {
-		hm.insert(i, 0);
-	}
-
-	hm
-}
-
-impl Launchpad {
-	pub fn new() -> Launchpad {
-		let (daw_conn, midi_conn) = initialize_output();
-
-		Launchpad {
-			color_map: empty_pad_state(),
-			daw_conn,
-			midi_conn,
-			midi_in: None,
-		}
-	}
-
-	pub fn send(&mut self, message: &[u8]) {
-		self.midi_conn.send(message).unwrap();
-	}
-
-	pub fn send_daw(&mut self, message: &[u8]) {
-		self.daw_conn.send(message).unwrap();
-	}
-
-	pub fn set_programmer_mode(&mut self, is_enabled: bool) {
-		let mode = if is_enabled { 1 } else { 0 };
-
-		self.send(&[240, 0, 32, 41, 2, 12, 14, mode, 247]);
-	}
-
-	pub fn light_on(&mut self, position: u8, color: u8) {
-		self.color_map.insert(position, color);
-
-		self.send(&[0b10010000, position, color]);
-	}
-
-	pub fn paint_static_grid(&mut self, grid: Vec<Vec<u8>>) {
-		let mut specs: Vec<u8> = vec![240, 0, 32, 41, 2, 12, 3];
-
-		for (y, row) in grid.into_iter().enumerate() {
-			for (x, col) in row.into_iter().enumerate() {
-				let note = position_to_note(x as u8, y as u8);
-
-				println!("({}, {}) -> {}", x, y, note);
-				specs.append(&mut vec![0, note, col]);
-			}
-		}
-
-		specs.push(247);
-
-		println!("colour spec: {:?}", specs);
-		self.send(&specs);
-	}
-
-	pub fn paint_rgb_grid(&mut self, grid: Vec<Vec<&Vec<u8>>>) {
-		let mut specs: Vec<u8> = vec![240, 0, 32, 41, 2, 12, 3];
-
-		for (y, row) in grid.into_iter().enumerate() {
-			for (x, col) in row.into_iter().enumerate() {
-				let note = position_to_note(x as u8, y as u8);
-				let mut spec = vec![3, note];
-
-				let mut colcl = col.clone();
-				spec.append(&mut colcl);
-
-				println!("({}, {}) -> {}", x, y, note);
-				specs.append(&mut spec);
-			}
-		}
-
-		specs.push(247);
-
-		println!("colour spec: {:?}", specs);
-		self.send(&specs);
-	}
-
-	pub fn cycle_color(&mut self, position: u8) {
-		let mut color = self.color_map.get(&position).unwrap_or(&0);
-		if color > &127 {
-			color = &0
-		}
-
-		self.light_on(position, color + 1);
-	}
-
-	pub fn setup(&mut self) {
-		// Enable Programmer Mode
-		self.set_programmer_mode(true);
-		println!("programmer mode enabled");
-	}
-
-	pub fn clear(&mut self, color: u8) {
-		for position in 11..100 {
-			self.light_on(position, color);
-		}
-	}
-}
-
-fn rand_u8() -> u8 {
-	let mut rng = rand::thread_rng();
-
-	rng.gen_range(80..87)
-}
 
 fn main() {
 	let mut launchpad = Launchpad::new();
 	launchpad.setup();
 
-	let input = midi_input("CommandPad MIDI Input");
-	let (_, midi_port) = get_midi_in_ports(&input);
-
 	let mut rng = rand::thread_rng();
 
-	let r = &vec![127, 0, 0];
-	let w = &vec![127, 127, 127];
-	let fps = 15;
+	launchpad.clear(1);
 
-	for iter in 1..(10 * fps) {
-		let b = &vec![
-			rng.gen_range(1..127),
-			rng.gen_range(1..127),
-			rng.gen_range(1..127),
-		];
+	let fps = 6;
 
-		launchpad.paint_rgb_grid(vec![
-			vec![r, r, r, r, r, r, r, r],
-			vec![r, r, r, r, r, r, r, r],
-			vec![w, w, w, w, w, w, w, w],
-			vec![b, b, b, b, b, b, b, b],
-			vec![b, b, b, b, b, b, b, b],
-			vec![w, w, w, w, w, w, w, w],
-			vec![r, r, r, r, r, r, r, r],
-			vec![r, r, r, r, r, r, r, r],
-		]);
+	let mut flappy = Flappy::new();
 
-		sleep(Duration::from_millis(1000 / fps))
-	}
+	let (tx, rx): (Sender<i32>, Receiver<i32>) = channel();
+
+	let input = midi_input("CommandPad MIDI Input");
+	let (_, midi_port) = get_midi_in_ports(&input);
 
 	let midi_in = input
 		.connect(
 			&midi_port,
 			"CommandPad MIDI Input",
-			move |timestamp_ms, message, _extra| {
-				println!("{:?}", message);
-
+			move |ms, message, _extra| {
 				if let &[command, position, velocity] = message {
-					if command == 176 && position == 97 && velocity > 0 {
-						launchpad.clear(0);
-						return;
-					}
-
-					if command == 176 && position == 98 && velocity > 0 {
-						for position in 11..100 {
-							launchpad.light_on(position, rand_u8());
-						}
-
-						return;
-					}
-
 					if command == 144 && velocity > 0 {
-						println!("note on at {}", position);
-
-						launchpad.cycle_color(position);
-					}
-
-					if command == 176 && velocity > 0 {
-						println!("ctrl note on at {}", position);
-
-						launchpad.cycle_color(position);
+						tx.send(100).unwrap();
 					}
 				}
 			},
@@ -290,6 +48,63 @@ fn main() {
 		.unwrap();
 
 	loop {
-		sleep(Duration::from_millis(50));
+		if flappy.is_dead {
+			launchpad.clear(4);
+			println!("Game Over! Your Score: {}", flappy.score);
+
+			loop {
+				let v = rx.recv().unwrap_or(-1);
+				if v == 100 {
+					println!("restarting...");
+					flappy.reset();
+
+					break;
+				}
+			}
+		}
+
+		let v = rx.try_recv().unwrap_or(-1);
+		flappy.jumping = v == 100;
+		flappy.tick();
+
+		let mut canvas = blank_rgb_canvas();
+
+		for (time, top_h, bottom_h) in flappy.obstacles.clone() {
+			if flappy.is_dead {
+				break;
+			}
+
+			if time < 1 {
+				continue;
+			}
+
+			for y in 0..top_h {
+				if time == 1 && y == flappy.char_y as u8 {
+					println!("colliding at top!");
+					flappy.is_dead = true;
+				}
+
+				canvas[y as usize][(time - 1) as usize] = vec![0, 127, 0];
+			}
+
+			for y in (9 - bottom_h)..9 {
+				if time == 1 && y - 1 == flappy.char_y as u8 {
+					println!("colliding at bottom!");
+					flappy.is_dead = true;
+				}
+
+				canvas[(y - 1) as usize][(time - 1) as usize] = vec![127, 0, 0];
+			}
+		}
+
+		canvas[flappy.char_y as usize][1] = if flappy.jumping {
+			vec![0, 0, 127]
+		} else {
+			vec![127, 0, 127]
+		};
+
+		launchpad.paint_rgb_grid(canvas);
+
+		sleep(Duration::from_millis(1000 / fps))
 	}
 }
